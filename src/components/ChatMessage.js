4987fda77a3aee937bb36ac73a37c9175ce1ad12
taylulz <taylulzcode@gmail.com>
2021-03-05 13:31:13 -0800

    successfully move ChatRoom to own component

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,9 +2,8 @@ import React from 'react';
 import firebase from '../firebase';
 
 function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
   const auth = firebase.auth();
-
+  const { text, uid, photoURL } = props.message;
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
