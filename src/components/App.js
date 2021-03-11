1f6c12542696c23832fa2f4c4fcedcc43faaf541
taylulz <taylulzcode@gmail.com>
2021-03-11 07:31:55 -0800

    stye photo and text message on one line

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import './App.css';
 import 'firebase/auth';
 import 'firebase/analytics';
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import firebase from '../firebase';
-// import SignIn from './SignIn';
-// import SignOut from './SignOut';
-// import ChatRoom from './ChatRoom';
-// import Login from './Login';
 import Navigation from './Navigation';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {Container} from 'react-bootstrap';
@@ -18,21 +12,10 @@ import Account from './Account';
 
 
 function App() {
-  // const auth = firebase.auth();
-  // const [user] = useAuthState(auth);
+
 
   return (
     <>
-      {/* <header>
-        <h1>My Chat App</h1>
-        <SignOut />
-      </header>*/}
-      
-      {/* <SignIn/>
-      <SignOut/>
-      <ChatRoom/> */}
-
-
       <Router>
         <Navigation/>
           <Container>
@@ -49,9 +32,6 @@ function App() {
             </Switch>
           </Container>
       </Router>
-
-      {/* <section>{user ? <> <SignOut/> <ChatRoom/> </> : <Login/>}</section>  */}
-      
     </>
   );
 }
