2d1f345b7535b188012ed7bb817c03ee9f46bf75
taylulz <taylulzcode@gmail.com>
2021-03-11 07:38:54 -0800

    remove non-working features from navbar

--- src/components/FriendRequests.js
+++ src/components/FriendRequests.js
@@ -1,46 +1,46 @@
-import { useFirestore, isLoaded } from "react-redux-firebase";
-import styled from "styled-components";
+// import { useFirestore, isLoaded } from "react-redux-firebase";
+// import styled from "styled-components";
 
-const FriendRequests = (props) => {
-  const firestore = useFirestore();
-  const { currentUser, pendingRequests} = props; 
+// const FriendRequests = (props) => {
+//   const firestore = useFirestore();
+//   const { currentUser, pendingRequests} = props; 
 
-  const usersRef = firestore.collection('users');
+//   const usersRef = firestore.collection('users');
 
-  const friendsRef = firestore
-    .collection('users')
-    .doc(currentUser.uid)
-    .collection('friends');
+//   const friendsRef = firestore
+//     .collection('users')
+//     .doc(currentUser.uid)
+//     .collection('friends');
 
-  const pendingRequestsRef = firestore
-    .collection('users')
-    .doc(currentUser.uid)
-    .collection("pendingRequests");
+//   const pendingRequestsRef = firestore
+//     .collection('users')
+//     .doc(currentUser.uid)
+//     .collection("pendingRequests");
 
-  const acceptRequest = (userEmail, id) => {
-    alert('accepted');
-    friendsRef.doc(userEmail).set({
-      email: userEmail,
-      id: id,
-    });
-    usersRef.doc(id).collection("friends").doc(currentUser.email).set({
-      email: currentUser.email,
-      id: currentUser.uid,
-    });
-    pendingRequestsRef
-      .doc(userEmail)
-      .delete()
-      .then(()=>{
-        console.log("deleted from requests");
-      });
-    usersRef
-      .doc(id)
-      .collection("sentRequests")
-      .doc(currentUser.email)
-      .delete()
-      .then(() => {
-        console.log('successfully deleted');
-      });
-  }
+//   const acceptRequest = (userEmail, id) => {
+//     alert('accepted');
+//     friendsRef.doc(userEmail).set({
+//       email: userEmail,
+//       id: id,
+//     });
+//     usersRef.doc(id).collection("friends").doc(currentUser.email).set({
+//       email: currentUser.email,
+//       id: currentUser.uid,
+//     });
+//     pendingRequestsRef
+//       .doc(userEmail)
+//       .delete()
+//       .then(()=>{
+//         console.log("deleted from requests");
+//       });
+//     usersRef
+//       .doc(id)
+//       .collection("sentRequests")
+//       .doc(currentUser.email)
+//       .delete()
+//       .then(() => {
+//         console.log('successfully deleted');
+//       });
+//   }
 
-}
+// }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,23 +6,14 @@ function Navigation() {
 return(
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
-      <Link to="/" className="navbar-brand" >ProjName</Link>
+      <Link to="/" className="navbar-brand">KatChat</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/ChatRoom">ChatRoom</Link>
-          </li>
-          <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/Account">Account</Link>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/DMs">PrivateMessages</Link>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/kittycube">Kitty Cube Easter Egg</Link>
           </li>
         </ul>
       </div>
